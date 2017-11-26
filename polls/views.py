95671c1fd3a7c7debdbc5444395090fd54510993
Stéphanie Bissonnette <paprika.49.sb@gmail.com>
2017-11-25 22:02:41 -0500

    new

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'polls/index.html', {})
+
+def target(request):
+    return render(request, 'polls/target.html', {})
